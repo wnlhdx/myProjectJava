7a5062a59873dfc3bedcb5ebd331b45a425b88e5
wnlhdx <314460007@qq.com>
2022-01-23 11:05:35 +0000

    phonefirst

---
+++ welcome.java
@@ -0,0 +1,11 @@
+public class welcome{
+  public static void main(String[] args){
+      String greeting = "welcome to Core java";
+      System.out.println(greeting);
+      for(int i=0;i<greeting.length();i++)
+          System.out.print("=");
+      System.out.println();
+  }
+}
+
+
