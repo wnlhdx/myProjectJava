52871e1527ecf21562650fb4e8b003b91796500d
wnlhdx <314460007@qq.com>
2022-01-28 15:00:43 +0000

    corejava2

--- coreJava/v1ch02/Welcome/welcome.java
+++ coreJava/v1ch02/Welcome/welcome.java
@@ -1,10 +1,11 @@
- gitpublic class welcome{
+public class welcome{
   public static void main(String[] args){
       String greeting = "welcome to Core java";
       System.out.println(greeting);
       for(int i=0;i<greeting.length();i++)
           System.out.print("=");
       System.out.println();
+      System.out.print();
   }
 }
 
